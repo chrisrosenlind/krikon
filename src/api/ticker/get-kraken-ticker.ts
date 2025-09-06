c712c546fd087cea0f136f820a8bbc24fadf6371
Christoffer <christoffer.rosenlind@gmail.com>
2025-09-06 10:25:41 +0200

    changed types

--- src/api/ticker/get-kraken-ticker.ts
+++ src/api/ticker/get-kraken-ticker.ts
@@ -1,5 +1,5 @@
 import axios from 'axios'
-import type { KrakenTickerResponse, TickerInfo } from './types.js'
+import type { KrakenTickerResponse, TickerInfo } from './kraken-types.js'
 import { ASSET_MAPPING } from 'src/utils/kraken-asset-mapping.js'
 
 const TICKER_URL = process.env.KRAKEN_BASE_URL + '/public/Ticker'
